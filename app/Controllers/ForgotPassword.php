a9e3f29dec847fd2cafaa3363b2278261b26ca64
Dang Cam <dangcam.pr@outlook.com>
2023-03-13 10:49:12 +0700

    Initial commit

---
+++ app/Controllers/ForgotPassword.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Controllers;
+
+
+use CodeIgniter\Controller;
+
+class ForgotPassword extends BaseController
+{
+    public function index()
+    {
+        echo 'forgot_password';
+    }
+}
